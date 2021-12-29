cf93ce4e4ad99b42ea881072d28607494bc4154b
sebride4988 <sebride4988@gmail.com>
2021-12-29 11:29:23 +0900

    refactor(hook): 익명 로그인 훅 삭제

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,4 +1,3 @@
 // @index('./*', f => `export { ${f.name} } from '${f.path}';`)
 export { useAuthState } from './useAuthState';
-export { useSignInAnonymously } from './useSignInAnonymously';
 // @endindex
